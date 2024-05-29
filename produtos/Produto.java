626f49e04ee7ff6be20da0f5f04051639702bc89
luanvinii <luanvnms2512@gmail.com>
2024-05-28 23:30:13 -0300

    up
    //Criação dos Getters e Setters

--- produtos/Produto.java
+++ produtos/Produto.java
@@ -6,4 +6,44 @@ class Produto{
     private String medida;
     private int quantidade;
     private String marca;
+    
+    //Criação dos Getters e Setters dos atributos de produto
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public String getMedida() {
+        return medida;
+    }
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    
 }
--- usuarios/Funcionario.java
+++ usuarios/Funcionario.java
@@ -3,4 +3,26 @@ class Funcionario {
     private int codigo;
     private String nome;
     private String cargo;
+    
+    //Criação dos Getters e Setters dos atributos de funcionario
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    
 }
