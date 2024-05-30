d10761a020f130c32b70480f343f4601155b7af6
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2024-05-30 12:09:49 -0300

    criação do projeto swing

---
+++ src/Conexao/DB.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author Vinicius
+ */
+public class DB {
+    
+    private static Connection conn = null;
+    
+    
+    public static Connection getConexao() {            
+        String url = "jdbc:sqlite:db-hotel";
+        
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(url);
+            } catch (SQLException ex) {    
+                 Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+        }
+        return conn;
+    }
+    
+    public static void closeConexao() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
+
+
---
+++ src/Models/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class Cliente {
+    
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
